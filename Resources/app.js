28183a211e744b4021119317c0fea1699b8f86a4
paperli <paper.li@gmail.com>
2013-08-26 23:24:39 +0800

    Determine whether it's new day or not to avoid over post

--- Resources/app.js
+++ Resources/app.js
@@ -15,6 +15,8 @@ if (Ti.version < 1.8 ) {
 	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
 }
 
+//Ti.App.Properties.setString('first_run',0);
+
 var db = Ti.Database.open('diaryQA');
 	
 	if (Ti.App.Properties.getString('first_run') == null){
@@ -23,7 +25,7 @@ var db = Ti.Database.open('diaryQA');
 		//create database when the app launch (or open it if exist)
 		
 		//max. 8 questions TODO: make flexibile
-		db.execute('CREATE TABLE IF NOT EXISTS chats (id INTEGER PRIMARY KEY,supervisor, date, q1, a1, q2, a2, q3, a3, q4, a4, q5, a5, q6, a6, q7, a7, q8, a8)');
+		db.execute('CREATE TABLE IF NOT EXISTS chats (id INTEGER PRIMARY KEY,supervisor, date, msg, type)');
 		//config
 		db.execute('CREATE TABLE IF NOT EXISTS configs (id INTEGER PRIMARY KEY, alert_time,supervisor,q1,q2,q3,q4,q5,q6,q7,q8)');
 		//insert default settings (FROM SUN. 1 to SAT. 7)
@@ -52,10 +54,10 @@ var db = Ti.Database.open('diaryQA');
 	var rows = db.execute('SELECT * FROM configs WHERE id='+(today.getDay()+1));
 	Ti.API.info("Today is "+today.getDay()+". Result has "+rows.rowCount+" rows. Revoke time is "+rows.fieldByName("alert_time")+". Id is "+rows.fieldByName("id"));
 	var revokeTimeArray = rows.fieldByName("alert_time").split(":");
-	var revokeTime = new Date();
+	var revokeTime = new Date(new Date().getTime()+60000);
 	//revokeTime.setHours(parseInt(revokeTimeArray[0]));
 	//revokeTime.setMinutes(parseInt(revokeTimeArray[1]));
-	revokeTime.setMinutes(42);
+	//revokeTime.setMinutes(42);
 	//revokeTime.setSeconds(0);
 	console.log(revokeTime.toDateString()+" "+revokeTime.toTimeString());
 	
@@ -83,6 +85,7 @@ var db = Ti.Database.open('diaryQA');
 	db.close();
 
 //TODO:background service (what will happen when app is paused)
+//TODO:It won't be revoked when the user goes back in app by pressing app icon directly instead of pressing the notification
 	Ti.App.iOS.addEventListener('notification',function(data){
 		var db = Ti.Database.open('diaryQA');
 		
@@ -90,11 +93,34 @@ var db = Ti.Database.open('diaryQA');
 		console.log(data.userInfo['q1']);
 		console.log(data.userInfo['supervisor']);
 		//Insert init question to databse
-		console.log('INSERT INTO chats (supervisor, date, q1) VALUES (?,?,?)',data.userInfo['supervisor'],data.userInfo['date'].toString(),data.userInfo['q1']);
-		db.execute('INSERT INTO chats (supervisor, date, q1) VALUES (?,?,?)',data.userInfo['supervisor'],data.userInfo['date'].toString(),data.userInfo['q1']);
+		//console.log('INSERT INTO chats (supervisor, date, q1) VALUES (?,?,?)',data.userInfo['supervisor'],data.userInfo['date'].toString(),data.userInfo['q1']);
+		//get last q1 msg in database and check if today existed
+		var lastQ1row = db.execute('SELECT * FROM chats WHERE type=\'q1\' ORDER BY id DESC LIMIT 1');
+		console.log("last q1 query has "+lastQ1row.rowCount+" result");
+		if (isNewDay(lastQ1row,data.userInfo['date'])){
+			console.log("It's new day thou!!");
+			db.execute('INSERT INTO chats (supervisor, date, msg, type) VALUES (?,?,?,?)',data.userInfo['supervisor'],data.userInfo['date'].toString(),data.userInfo['q1'],'q1');
+		} else {
+			console.log("......OP");
+		}
+		
 		db.close();
 	});
 
+
+function isNewDay(lastQ1,msgDate){
+	console.log(lastQ1.rowCount);
+	if (lastQ1.rowCount < 1 || lastQ1 == "undefined" || lastQ1.rowCount == null){
+		
+		//there is no data
+		return true;
+	}
+	var lastQ1Day = new Date(lastQ1.fieldByName('date'));
+	var msgDay = new Date(msgDate);
+	console.log("last: "+lastQ1Day.getDate());
+	console.log("now: "+msgDay.getDate());
+	return lastQ1Day.getDate() != msgDay.getDate();
+}
 
 // This is a single context application with mutliple windows in a stack
 (function() {
